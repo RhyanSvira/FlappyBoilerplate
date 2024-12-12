14f5d2cc112074a9bb4ed4ead801a00ba9d91b6e
RhyanSvira <melonman20x6@gmail.com>
2024-12-11 19:54:58 -0500

    text

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,4 +18,15 @@ public class Player : MonoBehaviour
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            FindObjectOfType<GameManager>().GameOver();
+        } else if (other.gameObject.tag == "Scoring")
+        {
+            FindObjectOfType<GameManager>().IncreaseScore();
+        }
+    }
 }
